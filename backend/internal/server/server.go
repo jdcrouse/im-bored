f9a88db77dd7ab1a084a383768e98083ce803e10
Jason Crouse <jdcrouse21@gmail.com>
2025-04-11 22:39:18 -0400

    only notify all users together

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -53,32 +53,6 @@ func (s *Server) handleRegisterUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-func (s *Server) handleNotifyUser(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	id := r.URL.Query().Get("id")
-	if id == "" {
-		http.Error(w, "ID is required", http.StatusBadRequest)
-		return
-	}
-
-	s.mu.RLock()
-	user, exists := s.registeredUsers[id]
-	s.mu.RUnlock()
-
-	if !exists {
-		http.Error(w, "ID not registered", http.StatusNotFound)
-		return
-	}
-
-	user.Notify()
-	response := Response{Message: "Notification sent for ID: " + id}
-	json.NewEncoder(w).Encode(response)
-}
-
 func (s *Server) handleListUsers(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received request to /users")
 	if r.Method != http.MethodGet {
@@ -98,9 +72,30 @@ func (s *Server) handleListUsers(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func (s *Server) handleNotifyAllUsers(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	s.mu.RLock()
+	users := make([]*user.User, 0, len(s.registeredUsers))
+	for _, user := range s.registeredUsers {
+		users = append(users, user)
+	}
+	s.mu.RUnlock()
+
+	for _, user := range users {
+		user.Notify()
+	}
+
+	response := Response{Message: "Notifications sent to all users"}
+	json.NewEncoder(w).Encode(response)
+}
+
 func (s *Server) Run(settings Settings) error {
 	http.HandleFunc("/register", s.handleRegisterUser)
-	http.HandleFunc("/notify", s.handleNotifyUser)
 	http.HandleFunc("/users", s.handleListUsers)
+	http.HandleFunc("/notify-all", s.handleNotifyAllUsers)
 	return http.ListenAndServe(settings.Endpoint, nil)
 }
